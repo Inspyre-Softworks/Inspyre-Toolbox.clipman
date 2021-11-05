3beef47fcf1d6922071582680a92fa137823a77f
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2021-11-04 21:38:15 -0500

    Update code
    * Added CHANGES file to track project changes
    * Added dependencies and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 about = {}
 
-with codecs.open(os.path.join("inspyre_toolbox", "clipman", "__init__.py")) as f:
+with codecs.open(os.path.join("inspyre_toolbox", "clipman", "__init__.py")) as f :
     exec(f.read(), about)
 
 with codecs.open('README.rst') as readme_file:
@@ -19,47 +19,61 @@ with codecs.open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-]
+        'inspyre-toolbox==1.0a7',
+        'inspy-logger==2.1a14'
+        ]
 
 extras = {
-    "docs": [
-        "sphinx", "furo",
-    ],
-    "tests": [
-        
-    ],
-}
+        "docs":  [
+                "sphinx", "furo",
+                ],
+        "tests": [
+                
+                ],
+        }
 
 setup(
-    name=about["__title__"],
-    author=about["__author__"],
-    author_email=about["__email__"],
-    version=about["__version__"],
-    description=about["__summary__"],
-    long_description=readme + '\n\n' + history,
-    long_description_content_type="text/x-rst",
-    license=about["__license__"],
-    url=about["__url__"],
-    keywords=["inspyre_toolbox.clipman",],
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    entry_points={
-        'console_scripts': [
-            'inspyre_toolbox.clipman=inspyre_toolbox.clipman.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    extras_require=extras,
-    include_package_data=True,
-    packages=find_packages(include=['inspyre_toolbox.clipman', 'inspyre_toolbox.clipman.*']),
-    zip_safe=False,
-)
+        name=about["__title__"],
+        author=about["__author__"],
+        author_email=about["__email__"],
+        version=about["__version__"],
+        description=about["__summary__"],
+        long_description=readme + '\n\n' + history,
+        long_description_content_type="text/x-rst",
+        license=about["__license__"],
+        url=about["__url__"],
+        keywords=["inspyre_toolbox.clipman", ],
+        python_requires='>=3.6',
+        classifiers=[
+                'Development Status :: 2 - Pre-Alpha',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: MIT License',
+                'Natural Language :: English',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+                'Programming Language :: Python :: 3.8',
+                'Environment :: GPU',
+                'Environment :: MacOS X',
+                'Environment :: Plugins',
+                'Environment :: X11 Applications',
+                'Intended Audience :: End Users/Desktop',
+                'Intended Audience :: System Administrators',
+                'License :: Freeware',
+                'Operating System :: Microsoft :: Windows :: Windows 10',
+                'Programming Language :: Python :: 3 :: Only',
+                'Topic :: System',
+                'Topic :: Utilities'
+                
+                ],
+        entry_points={
+                'console_scripts' : [
+                        'inspyre_toolbox.clipman=inspyre_toolbox.clipman.cli:main',
+                        ],
+                },
+        install_requires=requirements,
+        extras_require=extras,
+        include_package_data=True,
+        packages=find_packages(include=['inspyre_toolbox.clipman', 'inspyre_toolbox.clipman.*']),
+        zip_safe=False,
+        )
