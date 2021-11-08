ab6110df3b0914899a2e211d0366460c79d38fc5
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2021-11-08 17:59:34 -0600

    Refactor
    Signed-off-by: Taylor B <43686206+tayjaybabee@users.noreply.github.com>

--- inspyre_toolbox/__init__.py
+++ inspyre_toolbox/__init__.py
@@ -1,3 +1,6 @@
+#  Copyright (c) 2021. __init__.py is a part of the ''inspyre-toolbox.clipman' project by Inspyre Softworks D.O. and is licensed under the MIT License. Feel free to copy, share, distribute any code from this project  as you please. If you do; please share where you got it.
+
 from pkg_resources import declare_namespace
 
 declare_namespace(__name__)
+
