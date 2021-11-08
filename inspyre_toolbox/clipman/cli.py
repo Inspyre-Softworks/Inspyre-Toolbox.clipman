f6ad62a4d496aa27bc12fb83f720ecf3966c6ea7
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2021-11-08 17:58:46 -0600

    Create entry-point
    Signed-off-by: Taylor B <43686206+tayjaybabee@users.noreply.github.com>

--- inspyre_toolbox/clipman/cli.py
+++ inspyre_toolbox/clipman/cli.py
@@ -1,17 +1,25 @@
 """Console script for inspyre_toolbox.clipman."""
-import argparse
+#  Copyright (c) 2021. cli.py is a part of the ''inspyre-toolbox.clipman' project by Inspyre Softworks D.O. and is licensed under the MIT License. Feel free to copy, share, distribute any code from this project  as you please. If you do; please share where you got it.
+
 import sys
 
+from inspyre_toolbox.clipman.clipboard_history import listener_thread
+from inspyre_toolbox.clipman.config import ARGS
+from inspyre_toolbox.clipman.gui import run as run_gui
 
-def main():
+
+def main() :
     """Console script for inspyre_toolbox.clipman."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "inspyre_toolbox.clipman.cli.main")
+    
+    listener_thread.start()
+    
+    run_gui()
+    
+    print("Arguments: " + str(ARGS))
+    print(
+        "Replace this message by putting your code into "
+        "inspyre_toolbox.clipman.cli.main"
+        )
     return 0
 
 
