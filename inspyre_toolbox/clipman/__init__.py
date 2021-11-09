1c62e819ce42535d904f7c4b6cc8766952f96890
Taylor B <43686206+tayjaybabee@users.noreply.github.com>
2021-11-08 19:40:08 -0600

    Update packaging information
    Signed-off-by: Taylor B <43686206+tayjaybabee@users.noreply.github.com>

--- inspyre_toolbox/clipman/__init__.py
+++ inspyre_toolbox/clipman/__init__.py
@@ -1,10 +1,12 @@
 """Package for inspyre-toolbox.clipman."""
 
+#  Copyright (c) 2021-2021. __init__.py is a part of the ''inspyre-toolbox.clipman' project by Inspyre Softworks D.O. and is licensed under the MIT License. Feel free to copy, share, distribute any code from this project  as you please. If you do; please share where you got it.
+
 __title__ = "inspyre_toolbox.clipman"
-__summary__ = "Keep a specified number of previous clipboard entries"
+__summary__ = "Keep a specified number of previous clipboard_history entries"
 __url__ = "https://github.com/tayjaybabee/inspyre_toolbox.clipman"
 
-__version__ = "1.0"
+__version__ = '1.0.0-dev1'
 
 __author__ = "Taylor-Jayde Blackstone"
 __email__ = "t.blackstone@inspyre.tech"
